5e17e41094e17d45785b7eee4daff5782466d492
yagiharuka <84696162+yagiharuka@users.noreply.github.com>
2026-08-22 19:29:29 +0900

    Load reading-guide policy after company search renderer

--- pages-site/main.tsx
+++ pages-site/main.tsx
@@ -10,6 +10,7 @@ import "./company-search-alternatives.css";
 import "./company-evidence-ui.css";
 import "./subsidy-semantics-ui.css";
 import "./company-search-ui";
+import "./company-search-reading-guide";
 import "./company-search-alternatives";
 import "./company-evidence-ui";
 import "./subsidy-semantics-ui";
